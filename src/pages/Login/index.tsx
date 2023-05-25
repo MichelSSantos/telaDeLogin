d03f77331722b28ebcffbf40728775cb3688cc44
Michel Santos <michelshaiene2010@hotmail.com>
2023-05-24 23:11:43 -0300

    tela de login

---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,78 @@
+import { useForm } from "react-hook-form";
+import Button from "../../components/Button";
+import Input from "../../components/Input";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+import { Container, LoginContainer, Column, Spacing, Title } from "./styles";
+import { defaultValues, IFormLogin } from "./types";
+
+
+const schema = yup
+  .object({
+    email: yup.string()
+    .email("E-mail inválido")
+    .required("Campo obrigatório"),
+    password: yup
+      .string()
+      .min(6, "No minimo 6 caracteres")
+      .required("Campo obrigatório"),
+  })
+  .required();
+
+// const Login = () => {
+//   const {
+//     control,
+//     formState: { errors, isValid },
+//   } = useForm<IFormLogin>({
+//     resolver: yupResolver(schema),
+//     mode: "onBlur",
+//     defaultValues,
+//     reValidateMode: "onChange",
+//   });
+
+const Login = () => {
+  const {
+    control,
+    watch, 
+    formState:{errors, isValid},
+  } = useForm<IFormLogin>({
+    resolver: yupResolver(schema),
+    mode:'onBlur',
+    defaultValues,
+    reValidateMode: 'onChange',
+  });
+
+  const form = watch();
+  console.log(form)
+  return (
+    <Container>
+      <LoginContainer>
+        <Column>
+          <Title>Login</Title>
+          <Spacing />
+          <Input
+            name="email"
+            placeholder="Email"
+            //control={control}
+            control={control}
+            errorMessage={errors?.email?.message}
+          />
+          <Spacing />
+          <Input
+            name="password"
+            type="password"
+            placeholder="Senha"
+            // control={control}
+            control={control}
+            errorMessage={errors?.password?.message}
+          />
+          <Spacing />
+          <Button title="Entrar" />
+        </Column>
+      </LoginContainer>
+    </Container>
+  );
+};
+
+export default Login;
